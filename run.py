e32de87ee72a0366d939f87d37e57a5eefb1d92d
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2023-04-15 22:05:01 +0000

    add stock_row function to slice last row of stock and display

--- run.py
+++ run.py
@@ -88,8 +88,8 @@ def calculate_surplus_data(sales_row):
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    pprint(stock)
-
+    stock_row = stock[-1]
+    print(stock_row)    
 
 
 def main():
